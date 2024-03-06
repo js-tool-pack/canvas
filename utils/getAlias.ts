d5f119eb10a73f696416d7b4d065cfbfa331e70c
dyh_a <xinzon32177@163.com>
2024-03-06 22:13:26 +0800

    chore: 修复 alias

--- utils/getAlias.ts
+++ utils/getAlias.ts
@@ -3,14 +3,25 @@ import Fs from 'fs';
 
 export function getAlias(): Record<string, string> {
   const pkgsPath = Path.resolve(__dirname, '../packages');
+  const coreSrcPath = Path.resolve(pkgsPath, 'core/src');
 
-  const pkgs = Fs.readdirSync(pkgsPath) || [];
+  const pkgs = Fs.readdirSync(pkgsPath);
+  const coreSrcDirs = Fs.readdirSync(coreSrcPath).map((s) =>
+    s.replace(/\.ts$/, ''),
+  );
 
   return {
     '@tool-pack/canvas': Path.resolve(pkgsPath, 'canvas/src'),
     ...pkgs.reduce(
       (prev, cur) => {
         prev['@pkg/' + cur] = Path.resolve(pkgsPath, `${cur}/src`);
+        return prev;
+      },
+      {} as Record<string, string>,
+    ),
+    ...coreSrcDirs.reduce(
+      (prev, cur) => {
+        prev['~/' + cur] = Path.resolve(coreSrcPath, cur);
         return prev;
       },
       {} as Record<string, string>,
